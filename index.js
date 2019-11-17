ba732553f47f5076a31c97b704eef7148756b890
Omer Katan <omerkatan@live.com>
2019-11-16 23:15:59 -0800

    color and axios working

--- index.js
+++ index.js
@@ -2,20 +2,31 @@ const inquirer = require("inquirer");
 const axios = require("axios");
 
 inquirer
-    .prompt(
+    .prompt([
         {
             type: "message",
             message: "Input your GitHub username: ",
             name: "username"
-        }
-    ).then(function({username}) {
+        },
+        {
+            name: "color",
+            type: "list",
+            message: "What color template would you link for the resume?",
+            choices: ["green", "blue", "pink", "red"]
+        },
+    ]).then(function (answers) {
+        const color = answers.color;
+        const username = answers.username;
+        console.log(username);
+        console.log(color);
+
         const queryURL = `https://api.github.com/users/${username}`;
 
 
         axios
             .get(queryURL)
-            .then(function(response) {
-                // console.log(response);
+            .then(function (response) {
+                console.log(response);
                 const bio = response.data.bio;
                 const profilePic = response.data.avatar_url;
                 const username = response.data.login;
@@ -24,13 +35,6 @@ inquirer
                 const numRepos = response.data.public_repos;
                 const followers = response.data.followers;
                 const following = response.data.following;
-                console.log(bio);
-                console.log(profilePic);
-                console.log(username);
-                console.log(location);
-                console.log(profileURL);
-                console.log(numRepos);
-                console.log(followers);
-                console.log(following);
             });
     })
+
