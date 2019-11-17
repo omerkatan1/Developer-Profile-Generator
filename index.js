e70f0ba0ae8d350690379a76a6e5724c451188e6
Omer Katan <omerkatan@live.com>
2019-11-17 14:13:47 -0800

    url stuff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
+const fs = require("fs");
 
 inquirer
     .prompt([
@@ -15,22 +16,20 @@ inquirer
             choices: ["green", "blue", "pink", "red"]
         },
     ]).then(function (answers) {
-
-        function getColor(answers) {
-            const color = answers.color;
-            return color;
-        }
+        const color = answers.color;
         const username = answers.username;
         console.log(username);
         console.log(color);
 
         const queryURL = `https://api.github.com/users/${username}`;
 
-
+        // const appendFileAsync = util.promisify(fs.appendFile);
+        // const readFileAsync = util.promisify(fs.readFile);
 
         axios
             .get(queryURL)
             .then(function (response) {
+
                 const bio = response.data.bio;
                 const profilePic = response.data.avatar_url;
                 const username = response.data.login;
@@ -39,5 +38,26 @@ inquirer
                 const numRepos = response.data.public_repos;
                 const followers = response.data.followers;
                 const following = response.data.following;
+
+
+                const responseArray = [bio, profilePic, username, location, profileURL, numRepos, followers, following];
+
+
+                fs.writeFile('gitResponse.txt', "" , function(err) {
+                    if(err) {
+                        return console.log(err);                        
+                    }
+                    console.log("File saved!");
+                })
+                
+                for(var i = 0; i < responseArray.length; i++) {
+                    fs.appendFileAsync('gitResponse.txt', responseArray[i] + "\n", function(err) {
+                        if(err) {
+                            console.log(err);
+                        } else {
+                            console.log("Appended!");
+                        }
+                    })
+                }
             });
     })
